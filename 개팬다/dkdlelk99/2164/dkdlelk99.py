4b20161b06130e35f23b4a3d07b24d114071c2b5
unknown <32194363@dankook.ac.kr>
2021-07-27 19:54:11 +0900

    v2

--- 개팬다/dkdlelk99/2164/dkdlelk99.py
+++ 개팬다/dkdlelk99/2164/dkdlelk99.py
@@ -3,7 +3,14 @@ import sys
 N = int(sys.stdin.readline())
 deck = list(range(1, N+1))
 
+# while len(deck) != 1:
+#     deck.pop(0)
+#     deck.append(deck.pop(0))
+# print(deck[0])
 while len(deck) != 1:
-    deck.pop(0)
-    deck.append(deck.pop(0))
+    rec = list(range(0, len(deck), 2))
+    rec.reverse()
+    for i in rec:
+        deck.pop(i)
+    deck.reverse()
 print(deck[0])
