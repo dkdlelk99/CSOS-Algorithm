82b134b15bd41ec5659bf795c139021a59ea7b04
unknown <32194363@dankook.ac.kr>
2021-07-27 19:54:45 +0900

    v3

--- 개팬다/dkdlelk99/2164/dkdlelk99.py
+++ 개팬다/dkdlelk99/2164/dkdlelk99.py
@@ -1,16 +1,12 @@
 import sys
 #Input
 N = int(sys.stdin.readline())
-deck = list(range(1, N+1))
+# deck = list(range(1, N+1))
 
-# while len(deck) != 1:
-#     deck.pop(0)
-#     deck.append(deck.pop(0))
-# print(deck[0])
-while len(deck) != 1:
-    rec = list(range(0, len(deck), 2))
-    rec.reverse()
-    for i in rec:
-        deck.pop(i)
-    deck.reverse()
-print(deck[0])
+ans = 0
+for i in range(17):
+    if N >= 2**(i+1):
+        ans += 1
+    else:
+        break
+print(2**ans)
