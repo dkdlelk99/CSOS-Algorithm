cce4d4ac834c6822f9ba01d60abefb5b94913660
dkdlelk99 <dkdlelk99@gmail.com>
2021-08-04 14:34:36 +0900

    요세푸스 문제
    deque 문제였다.
    사실 어제 문제 쉬워서 어제부터 풀기 시작했는데 2일 걸림 ㅎ

---
+++ BOJ/11866/dkdlelk99.py
@@ -0,0 +1,10 @@
+import sys, collections
+# input
+N, K = map(int, sys.stdin.readline().split())
+josephus = collections.deque(range(1, N+1))
+answer = "<"
+for _ in range(N): # N개의 숫자열을 만든다
+    for k in range(K-1): # k-1번 돌리고
+        josephus.append(josephus.popleft())
+        answer += str(josephus.popleft()) + ", " # 뽑아서 넣는다
+print(answer[:-2]+">")
