f712a06fc4aa57fa449b1ceca74d72831a039c38
backmurry <40791021+backmurry@users.noreply.github.com>
2021-09-04 08:09:44 +0900

    Update backmurry.cpp
    3항 연산자 및 배열 중 0을 버리면 보다 간단하게 코딩 가능

--- BOJ/1244/backmurry.cpp
+++ BOJ/1244/backmurry.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, std, g, y, i, k;
 
+    cin >> n;
+    int *arr = new int[n+1];
+
+    for (i = 1; i <= n; i++)cin >> arr[i];
+    cin >> std;
+    for (k = 0; k < std; k++)
+    {
+        cin >> g >> y; 
+        if (g == 1)for (i = 1; i <= n; i++)if(i % y == 0)arr[i] = arr[i]?0:1; 
+        if (g == 2)
+        {
+            arr[y] = arr[y]?0:1;                        
+            for (i = 1; arr[y + i] == arr[y - i]; i++) 
+            {
+                if (y + i > n || y - i < 1) 
+                    break;
+                arr[y + i] = arr[y + i]?0:1; 
+                arr[y - i] = arr[y - i]?0:1; 
+            }
+        }
+    }
+    for (i = 1; i <= n; i++)
+    {
+        cout << arr[i] << " ";
+        if (i % 20 == 0) 
+            cout << "\n";
+    }
+
+    return 0;
+}
