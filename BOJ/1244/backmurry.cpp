07f6629ba9264e04e185f40d663c8ecbe84d0e8c
backmurry <40791021+backmurry@users.noreply.github.com>
2021-09-04 08:14:39 +0900

    Update backmurry.cpp

--- BOJ/1244/backmurry.cpp
+++ BOJ/1244/backmurry.cpp
@@ -2,30 +2,30 @@
 using namespace std;
 int main()
 {
-    int n, std, g, y, i, k;
+    int switch_num, std, sex, number, i, k;
 
-    cin >> n;
-    int *arr = new int[n+1];
+    cin >> switch_num; 
+    int *arr = new int[switch_num+1];
 
-    for (i = 1; i <= n; i++)cin >> arr[i];
+    for (i = 1; i <= switch_num; i++)cin >> arr[i];
     cin >> std;
     for (k = 0; k < std; k++)
     {
-        cin >> g >> y; 
-        if (g == 1)for (i = 1; i <= n; i++)if(i % y == 0)arr[i] = arr[i]?0:1; 
-        if (g == 2)
+        cin >> sex >> number; 
+        if (sex == 1)for (i = 1; i <= switch_num; i++)if(i % number == 0)arr[i] = arr[i]?0:1; //배수마다 스위치 변경 
+        if (sex == 2)
         {
-            arr[y] = arr[y]?0:1;                        
-            for (i = 1; arr[y + i] == arr[y - i]; i++) 
+            arr[number] = arr[number]?0:1;                        
+            for (i = 1; arr[number + i] == arr[number - i]; i++) 
             {
-                if (y + i > n || y - i < 1) 
+                if (number + i > switch_num || number - i < 1) 
                     break;
-                arr[y + i] = arr[y + i]?0:1; 
-                arr[y - i] = arr[y - i]?0:1; 
+                arr[number + i] = arr[number + i]?0:1; 
+                arr[number - i] = arr[number - i]?0:1; 
             }
         }
     }
-    for (i = 1; i <= n; i++)
+    for (i = 1; i <= switch_num; i++)
     {
         cout << arr[i] << " ";
         if (i % 20 == 0) 
