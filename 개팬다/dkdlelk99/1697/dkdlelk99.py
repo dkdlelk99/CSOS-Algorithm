d7e97d08d87e08d0350f7e445b245efd83c27543
unknown <32194363@dankook.ac.kr>
2021-09-06 19:27:35 +0900

    숨바꼭질 실패 2
    런타임 에러
    
    아까껀 메모리 초과

--- 개팬다/dkdlelk99/1697/dkdlelk99.py
+++ 개팬다/dkdlelk99/1697/dkdlelk99.py
@@ -2,19 +2,19 @@ import sys, collections
 # input
 N, K = map(int, sys.stdin.readline().split())
 que = collections.deque()
-visited = {N: 0}
-
+visited = [-1] * 100001
+visited[N] = 0
 que.append(N)
 while len(que):
     parent = que.popleft()
     if parent != K:
-        if (parent * 2) not in visited:
+        if visited[parent * 2] < 0:
             que.append(parent * 2)
             visited[parent * 2] = visited[parent] + 1
-        if (parent + 1) not in visited:
+        if visited[parent + 1] < 0:
             que.append(parent + 1)
             visited[parent + 1] = visited[parent] + 1
-        if (parent - 1) not in visited:
+        if visited[parent - 1] < 0:
             que.append(parent - 1)
             visited[parent - 1] = visited[parent] + 1
     else:
