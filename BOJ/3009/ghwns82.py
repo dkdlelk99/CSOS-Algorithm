d0c3caf38638591c1008a1e23f2abe89d8e0e550
ghwns82 <68892482+ghwns82@users.noreply.github.com>
2021-09-06 11:50:32 +0900

    Delete ghwns82.py

--- BOJ/3009/ghwns82.py
+++
@@ -1,8 +0,0 @@
-a1,a2 = map(int, input().split())
-b1,b2 = map(int, input().split())
-c1,c2 = map(int, input().split())
-
-x = [a1,b1,c1]
-y = [a2,b2,c2]
-
-print(2*sum(set(x)) - sum(x) ,2*sum(set(y)) - sum(y))
