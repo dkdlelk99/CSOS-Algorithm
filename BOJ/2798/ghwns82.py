062f2791258269281a30fa77bd98d3f151fa33d9
ghwns82 <68892482+ghwns82@users.noreply.github.com>
2021-09-09 04:55:28 +0900

    Update ghwns82.py

--- BOJ/2798/ghwns82.py
+++ BOJ/2798/ghwns82.py
@@ -1,14 +1,6 @@
-# 2798
-
 from itertools import combinations
 
 N, M= map(int, input().split())
 card = list(map(int, input().split()))
 comb_ = [sum(i) for i in list(combinations(card, 3)) if sum(i) <= M]
-print(
-    sorted(list(set(comb_)))[-1]
-)
-
-# print(
-#     sorted(list(set([sum(i) for i in list(combinations(card, 3)) if sum(i) <= M])))[-1]
-# )
+print( sorted(list(set(comb_)))[-1] )
