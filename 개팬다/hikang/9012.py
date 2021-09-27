2d7463f200128ca6928b394f34e020f8e819ddd3
kanghaein <>
2021-09-27 13:36:50 +0000

    괄호 문제 완료

---
+++ 개팬다/hikang/9012.py
@@ -0,0 +1,14 @@
+import sys
+
+T = int(sys.stdin.readline())
+
+# '('랑 ')' 숫자 같으면 YES 출력되게
+for i in range(T):
+  list = sys.stdin.readline()
+  for j in list:
+    left="("
+    right=")"
+  if(list.count(left)==list.count(right)):
+    print("YES")
+  else:
+    print("NO")
