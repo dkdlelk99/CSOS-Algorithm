364b63b08d83ab343f639ff1556eaeded4f1e6ff
kanghaein <55188802+kanghaein@users.noreply.github.com>
2021-11-10 23:51:51 +0900

    Update hikang.py

--- BOJ/17140/hikang.py
+++ BOJ/17140/hikang.py
@@ -1,14 +1,14 @@
 import sys
-import time
 from collections import Counter
 
 
 
-# r, c, k 입력(첫쨋줄)
+# r, c, k 입력(첫쨋줄 입력)
 r, c, k = map(int,sys.stdin.readline().split())
-# 3X3 배열 입력(둘쨋줄)
-for _ in range(3):
-  A = list(map(int,sys.stdin.readline().split()))
+# 3X3 배열 입력(둘쨋줄 입력)
+A = [list(map(int,sys.stdin.readline().split())) for _ in range(3)]
+
+
 
 # R 연산
 def R():
@@ -28,16 +28,25 @@ def R():
   for ss in copy_A:
     ss += [0] * (max_len-len(ss))
   return copy_A
-# C 연산은 언제하는지 모르겠음..
-# def C():
 
-
-# 코드 실행 시작점
-start = time.time()
-
-# A[r][c] 연산을 시작
-def main():
-  if A[r][c] == k and (time.time()-start <= 10000):
-    print("소요된 시간은 : ",time.time()-start)
+time = 0
+while 1:
+  if time > 100:
+    break
+  # A[r-1][c-1] == k 인 경우에
+  
+  if r<=len(A) and c<=len(A[0]) and A[r-1][c-1] == k:
+    break
+  if len(A)>=len(A[0]):
+    A = R()
   else:
-    print("-1")
+    A = list(map(list,zip(*A)))
+    A = R()
+    A = list(map(list,zip(*A)))
+  
+  time += 1
+if time > 100:
+  print(-1)
+else:
+  print(time)
+
