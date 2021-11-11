576ad1471898d8ad081a75eae09d5309d4895793
dkdlelk99 <49552766+dkdlelk99@users.noreply.github.com>
2021-11-11 17:02:51 +0900

    Create dkdlelk99.py

---
+++ BOJ/15652/dkdlelk99.py
@@ -0,0 +1,16 @@
+import sys
+# input
+N, M = map(int, sys.stdin.readline().split())
+
+def recursive(n, picked, depth=M):
+    if depth == 0:
+        print(*picked)
+        return 0
+    next_pick = 1 if len(picked) == 0 else picked[-1]
+    for i in range(next_pick, n+1):
+        picked.append(i)
+        recursive(n, picked, depth - 1)
+        picked.pop()
+
+
+recursive(N, [])
