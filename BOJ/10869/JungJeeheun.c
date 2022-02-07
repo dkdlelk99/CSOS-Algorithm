0104f5a34290315f9ca614432ef9c88f9e54f7be
JungJeeheun <97943144+JungJeeheun@users.noreply.github.com>
2022-02-07 18:58:12 +0900

    Create JungJeeheun.c

---
+++ BOJ/10869/JungJeeheun.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int A, B;
+
+	scanf("%d %d", &A, &B);
+
+	printf("%d\n%d\n%d\n%d\n%d", A + B, A - B, A * B, A / B, A % B);
+	return 0;
+}
