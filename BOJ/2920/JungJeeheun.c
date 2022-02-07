5a6699cef6ae566312019a129098fa2228f315b0
JungJeeheun <97943144+JungJeeheun@users.noreply.github.com>
2022-02-07 18:45:13 +0900

    Create JungJeeheun.c

---
+++ BOJ/2920/JungJeeheun.c
@@ -0,0 +1,30 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int asc[8] = { 1,2,3,4,5,6,7,8 }, des[8] = { 8,7,6,5,4,3,2,1 };
+	int space[8];
+	int i, j = 0, k = 0;
+
+	for (i = 0; i < 8; i++)
+		scanf("%d", &space[i]);
+
+	while (space[j] == asc[j]) {
+		j++;
+		if (j == 8)
+			break;
+	}
+	while (space[k] == des[k]) {
+		k++;
+		if (k == 8)
+			break;
+	}
+	if (j == 8)
+		printf("ascending");
+	else if (k == 8)
+		printf("descending");
+	else
+		printf("mixed");
+	return 0;
+}
